825cc170f14f9c27e79d9c9db72ebf375baa746c
Klug007 <121226001+Klug007@users.noreply.github.com>
2024-01-23 15:17:10 +0500

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-export const host = "http://localhost:5001";
-// export const host = "https://metod.hadyasweets.uz/";
+// export const host = "http://localhost:5001";
+export const host = "https://metod.hadyasweets.uz/";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
